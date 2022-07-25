b8538f24a39da0d8210154264d9d887ba8180f14
Pedriana <pedrianapavao@gmail.com>
2022-07-25 10:59:27 -0300

    desafio concluído

--- src/index.js
+++ src/index.js
@@ -10,19 +10,59 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;  
+  
+  const user = users.find(user => user.username === username);
+  if(!user) {
+    return response.status(404).json({error:"User not found"});
+  }
+  request.user = user;
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request;
+
+  if (user.pro){ // pro = true
+    return next();
+  }
+  if (user.todos.length===10) {//length > 10
+    return response.status(403).json({error:"Maximum limit for the free plan is ten."})
+  }else{ 
+    return next();
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+  const user = users.find(user=>user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error:"user not found"});
+  }
+  if(validate(id)){
+    const todo = user.todos.find(todo=>todo.id === id);
+    if (todo){
+      request.user = user;
+      request.todo = todo;
+      return next();
+    }else{
+      return response.status(404).json({error:"todo not found"})
+    }
+  }else{
+    return response.status(400).json({error:"id not is uuid"})
+  }
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+  const user = users.find(user=>user.id === id);
+  if(!user){
+    return response.status(404).json({error:"user not found"})
+  }
+  request.user=user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
@@ -67,7 +107,6 @@ app.patch('/users/:id/pro', findUserById, (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   const { user } = request;
-
   return response.json(user.todos);
 });
 
